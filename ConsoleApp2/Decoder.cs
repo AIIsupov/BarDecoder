0e5639dd6190ffb32ec9b9046e5a8342cb46ed76
Александр <Aiisupov.Work@gmail.com>
2021-04-19 16:19:14 +0300

    Update 1.0.0.5

--- ConsoleApp2/Decoder.cs
+++ ConsoleApp2/Decoder.cs
@@ -91,13 +91,23 @@ namespace BarDecoder
                 string[] ports = SerialPort.GetPortNames();
                 if (ports.Length != 0)
                 {
-                    //Console.WriteLine(nameBarDec + "Доступные порты");
-                    //for (int i = 0; i < ports.Length; i++)
-                    //{
-                    //    Console.WriteLine("{0}: {1}", i, ports[i]);
-                    //}
-                    //Console.Write(nameBarDec + "Выберите порт:> ");
-                    //var pi = int.Parse(Console.ReadLine());
+                    /*Console.WriteLine(nameBarDec + "Доступные порты");
+                    for (int i = 0; i < ports.Length; i++)
+                    {
+                        Console.WriteLine("{0}: {1}", i, ports[i]);
+                    }
+                    Console.Write(nameBarDec + "Выберите порт:> ");
+                    var pi = int.Parse(Console.ReadLine());
+                    var SerPort = new SerialPort();
+                    SerPort.PortName = ports[pi];
+                    SerPort.Open();
+                    SerPort.DtrEnable = true;
+                    Console.WriteLine(nameBarDec + "{0} Открыт", SerPort.PortName);
+                    SerPort.DataReceived += Receiver;
+                    SerPort.ErrorReceived += ErrorReceived;
+                    SerPort.PinChanged += PinChanged;
+                    _port = new List<SerialPort>();
+                    _port.Add(SerPort);*/
 
                     _port = new List<SerialPort>();
                     for (int i = 0; i < ports.Length; i++)
@@ -107,6 +117,7 @@ namespace BarDecoder
                         {
                             _port[i].PortName = ports[i];
                             _port[i].Open();
+                            _port[i].DtrEnable = true;
                             Console.WriteLine(nameBarDec + "{0} Открыт", _port[i].PortName);
                             _port[i].DataReceived += Receiver;
                             _port[i].ErrorReceived += ErrorReceived;
@@ -154,98 +165,138 @@ namespace BarDecoder
 
         private void Receiver(object sender, SerialDataReceivedEventArgs e)
         {
-            Console.WriteLine(nameBarDec + "Получены данные");
+            Console.WriteLine(Environment.NewLine + nameBarDec + "Получение данных | {0}", DateTime.Now.ToLocalTime().ToString());
             if (e.EventType != SerialData.Eof)
             {
                 IsBarcodeDecode = false;
                 byte[] buf = null;
                 foreach (SerialPort port in _port)
-                {
+                {                    
                     if (port.BytesToRead != 0)
                     {
-                        int bytesToRead = port.BytesToRead;
-                        buf = new byte[bytesToRead];
-
-                        port.Read(buf, 0, bytesToRead);
-
-                        validBarcode = BarcodeVersions.IsValidBarcode(buf);
-                        BarcodeVersion barcodeVersion = BarcodeVersions.GetBarcodeVersion(buf);
-
-                        if (validBarcode)
+                        Console.WriteLine(nameBarDec + "Данные c {0}:", port.PortName);                        
+                        try
                         {
-                            int offset = 0;
-                            object[] values = new object[7]
-                            {
-                    GetObject(buf, _cnvs[typeof (NumberConverter)], typeof (byte), ref offset),
-                    GetObject(buf, _cnvs[typeof (NumberConverter)], typeof (ulong), ref offset),
-                    GetObject(buf, _cnvs[typeof (OMS62EncodingStringConverter)], typeof (string), ref offset, 51),
-                    GetObject(buf, _cnvs[typeof (NumberConverter)], typeof (byte), ref offset),
-                    GetObject(buf, _cnvs[typeof (ShortBirthDateConverter)], typeof (DateTime), ref offset),
-                    GetObject(buf, _cnvs[typeof (ShortBirthDateConverter)], typeof (DateTime), ref offset),
-                    ((IEnumerable<byte>) buf).ToList<byte>().GetRange(offset, barcodeVersion.Length - offset).ToArray() // электронная подпись
-                            };
+                            int bytesToRead = port.BytesToRead;
+                            buf = new byte[bytesToRead];
 
-                            string[] fullname = values[2].ToString().Split('|');
-                            BarcodeData = new BarcodeValue
-                            {
-                                code_type_Barcode = Convert.ToInt32(values[0]),
-                                number_Policy = Convert.ToInt64(values[1]),
-                                FullName = String.Format("{0} {1} {2}", fullname[0], fullname[1], fullname[2]),
-                                Surname = fullname[0],
-                                Name = fullname[1],
-                                Middle_Name = fullname[2],
-                                Gender = Convert.ToInt32(values[3]),
-                                Date_of_Birth = Convert.ToDateTime(values[4]).ToShortDateString(),
-                                _Date = Convert.ToDateTime(values[5]).ToShortDateString(),
-                                Bytecode = (byte[])values[6]
-                            };
+                            port.Read(buf, 0, bytesToRead);
 
-                            Console.WriteLine(Environment.NewLine + DateTime.Now.ToLocalTime().ToString() + " " + nameBarDec + "Полученные данные от {0}:", port.PortName);
-                            Console.WriteLine(nameBarDec + BarcodeData.code_type_Barcode);
-                            Console.WriteLine(nameBarDec + BarcodeData.number_Policy);
-                            Console.WriteLine(nameBarDec + BarcodeData.FullName);
-                            Console.WriteLine(nameBarDec + BarcodeData.Gender);
-                            Console.WriteLine(nameBarDec + BarcodeData.Date_of_Birth);
-                            Console.WriteLine(nameBarDec + BarcodeData._Date);
-                            Console.WriteLine(nameBarDec + BitConverter.ToString(BarcodeData.Bytecode));
+                            validBarcode = BarcodeVersions.IsValidBarcode(buf);
+                            BarcodeVersion barcodeVersion = BarcodeVersions.GetBarcodeVersion(buf);
 
-                            try
+                            if (validBarcode)
                             {
-                                onBarcodeDecode(this, BarcodeData);
+                                #region Декодирование
+                                int offset = 0;
+                                object[] values = new object[7]
+                                {
+                                    GetObject(buf, _cnvs[typeof (NumberConverter)], typeof (byte), ref offset),
+                                    GetObject(buf, _cnvs[typeof (NumberConverter)], typeof (ulong), ref offset),
+                                    GetObject(buf, _cnvs[typeof (OMS62EncodingStringConverter)], typeof (string), ref offset, 51),
+                                    GetObject(buf, _cnvs[typeof (NumberConverter)], typeof (byte), ref offset),
+                                    GetObject(buf, _cnvs[typeof (ShortBirthDateConverter)], typeof (DateTime), ref offset),
+                                    GetObject(buf, _cnvs[typeof (ShortBirthDateConverter)], typeof (DateTime), ref offset),
+                                    ((IEnumerable<byte>) buf).ToList<byte>().GetRange(offset, barcodeVersion.Length - offset).ToArray() // электронная подпись
+                                };
+                                #endregion
+
+                                string[] fullname = values[2].ToString().Split('|');
+                                BarcodeData = new BarcodeValue
+                                {
+                                    code_type_Barcode = Convert.ToInt32(values[0]),
+                                    number_Policy = Convert.ToInt64(values[1]),
+                                    FullName = String.Format("{0} {1} {2}", fullname[0], fullname[1], fullname[2]),
+                                    Surname = fullname[0],
+                                    Name = fullname[1],
+                                    Middle_Name = fullname[2],
+                                    Gender = Convert.ToInt32(values[3]),
+                                    Date_of_Birth = Convert.ToDateTime(values[4]).ToShortDateString(),
+                                    _Date = Convert.ToDateTime(values[5]).ToShortDateString(),
+                                    Bytecode = (byte[])values[6]
+                                };
+
+                                Console.WriteLine(nameBarDec + "code_type_Barcode: " + BarcodeData.code_type_Barcode);
+                                Console.WriteLine(nameBarDec + "number_Policy: " + BarcodeData.number_Policy);
+                                Console.WriteLine(nameBarDec + "FullName: " + BarcodeData.FullName);
+                                Console.WriteLine(nameBarDec + "Gender: " + BarcodeData.Gender);
+                                Console.WriteLine(nameBarDec + "Date_of_Birth: " + BarcodeData.Date_of_Birth);
+                                Console.WriteLine(nameBarDec + "_Date: " + BarcodeData._Date);
+                                Console.WriteLine(nameBarDec + "Bytecode: " + BitConverter.ToString(BarcodeData.Bytecode));
+
+                                try
+                                {
+                                    onBarcodeDecode(this, BarcodeData);
+                                }
+                                catch (NullReferenceException ex)
+                                {
+                                    Console.WriteLine(nameBarDec + ex.Message);
+                                }
+                                IsBarcodeDecode = true;
+                                port.DiscardInBuffer();
                             }
-                            catch (NullReferenceException ex)
-                            {
-                                Console.WriteLine(nameBarDec + ex.Message);
-                            }
-                            IsBarcodeDecode = true;
-                            port.DiscardInBuffer();
                         }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(nameBarDec + "Ошибка во время декодирования: " + ex.Message);
+                        }                        
                     }
                 }
             }
             else
             {
-                Console.WriteLine("Gabela");
-                Console.WriteLine(nameBarDec + @"Нет, это не данные!");
+                Console.WriteLine(nameBarDec + "Нет, это не данные!");
             }
         }
 
 
         private void ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
         {
-            foreach(SerialPort port in _port)
+            Console.WriteLine(Environment.NewLine + nameBarDec + "Поймал ошибку! {0}", DateTime.Now.ToLocalTime().ToString());
+            foreach (SerialPort port in _port)
             {
                 if (port.BytesToRead != 0)
                 {
-                    Console.WriteLine(nameBarDec + "ОГО \n" + port.ReadLine());
-                }                
+                    Console.WriteLine(nameBarDec + "Ошибка с {0}:\n" + port.ReadLine(), port.PortName);
+                }
             }                
         }
 
 
         private void PinChanged(object sender, SerialPinChangedEventArgs e)
         {
-            Console.WriteLine(nameBarDec + "Это что-то другое");
+            Console.WriteLine(Environment.NewLine + nameBarDec + "PinChanged! {0}", DateTime.Now.ToLocalTime().ToString());
+            foreach (SerialPort port in _port)
+            {
+                if (e.EventType == SerialPinChange.Break)
+                {
+                    Console.WriteLine(nameBarDec + "{0} Обнаружен разрыв на входе", port.PortName);
+                }
+                if (e.EventType == SerialPinChange.CDChanged)
+                {
+                    Console.WriteLine(nameBarDec + "{0} Изменилось состояние сигнала обнаружения несущей (CD). Этот сигнал используется для того, чтобы показать, что модем подключен к рабочей телефонной линии и в ней обнаружен сигнал несущей частоты.", port.PortName);
+                }
+                if (e.EventType == SerialPinChange.CtsChanged)
+                {
+                    Console.WriteLine(nameBarDec + "{0} Изменилось состояние сигнала готовности к приему (CTS). Этот сигнал используется для того, чтобы показать, что данные можно передавать через последовательный порт.", port.PortName);
+                }
+                if (e.EventType == SerialPinChange.DsrChanged)
+                {
+                    if (port.DsrHolding == false)
+                    {
+                        Console.WriteLine(nameBarDec + "{0} не готов к работе", port.PortName);
+                    }
+                    else
+                    {
+                        Console.WriteLine(nameBarDec + "{0} готов к работе", port.PortName);
+                    }
+                }
+                if (e.EventType == SerialPinChange.Ring)
+                {
+                    Console.WriteLine(nameBarDec + "{0} Обнаружен индикатор вызова", port.PortName);
+                }
+            }
+            
         }
 
         private static object GetObject(byte[] data,ITypeConverter converter,Type type,ref int offset)
